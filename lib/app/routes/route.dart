62038e980f757376b14713261cba69add16ceba4
Robin Hood <yoga3911@gmail.com>
2022-04-01 17:47:56 +0700

    add custom routes

--- lib/app/routes/route.dart
+++ lib/app/routes/route.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:sulai/app/views/home/home.dart';
+
+import '../views/auth/login.dart';
+
 class Routes {
-  
+  Routes._();
+
+  static const home = "/home";
+  static const login = "/login";
+
+  static final data = <String, WidgetBuilder>{
+    home: (_) => const HomePage(),
+    login: (_) => const LoginPage(),
+  };
 }
