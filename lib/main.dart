3a70b45eb884e522273b2c312cb4ddb2845fc150
Robin Hood <yoga3911@gmail.com>
2022-04-15 17:13:39 +0700

    add color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +21,11 @@ class MyApp extends StatelessWidget {
         initialRoute: Routes.login,
         routes: Routes.data,
         theme: ThemeData(
-          fontFamily: "Poppins"
-        ),
+            colorScheme: const ColorScheme.light(
+              primary: Color.fromARGB(255, 255, 219, 134),
+              onSurface: Colors.black,
+            ),
+            fontFamily: "Poppins"),
       ),
     );
   }
